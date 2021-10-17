a2e3eab7e34821641a9f71b1af7516d7798ef249
Peter Molnar <petermolnar.hu@gmail.com>
2021-10-17 16:55:07 +0200

    custom containers with ViewBuilder

---
+++ ViewsAndModifiers/GridStackContentView.swift
@@ -0,0 +1,50 @@
+//
+//  GridStackContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Peter Molnar on 17/10/2021.
+//
+
+import SwiftUI
+
+struct GridStackContentView: View {
+    var body: some View {
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+                .padding(2)
+            Text("R\(row) C\(col)")
+                .font(.system(.caption, design: .monospaced))
+        }
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    // ViewBuilder
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct GridStackContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridStackContentView()
+    }
+}
